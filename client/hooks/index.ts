cafd0e0b46347c71ce810a4130cac7f03bdd8516
Ivan Kozlovskyi <ivan.kz5565@gmail.com>
2025-06-27 15:13:51 +0300

    delete dublicate

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useEntity } from './useEntity'
 export { useActions } from './useActions'
 export { useAcl } from './useAcl'
-export { useMenu } from './useMenu'
 export { usePageItems } from './usePageItems'
--- client/hooks/useMenu.ts
+++
@@ -1,7 +0,0 @@
-import { IMenu } from "../types";
-import { useAcl } from "./useAcl";
-
-export function useMenu(menu: IMenu, strict?: boolean): IMenu {
-  const { allow } = useAcl();
-  return Object.fromEntries(Object.entries(menu).filter(([key, value]) => (value.grant ? allow(value.grant, key) : !strict)))
-}
